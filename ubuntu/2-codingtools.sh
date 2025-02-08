870e049f7870793a890d31c0c1a0c0a5653ec58d
Noctsol <39346808+Noctsol@users.noreply.github.com>
2025-02-07 22:48:24 -0700

    Updating all scripts for Ubuntu2404 (#1)
    **Major**
    - ubuntu/1-initialinstall.sh - Added a script that sets up a bunch of major tools and dependencies that I use on ubuntu
    - ubuntu/2-codingtools.sh - Added a script to install all the coding tools that I'm likely to use
      - ubuntu/ref/rcfileappends.txt - Added a text file that contains items I append to my rcfiles
    
    **Minor**
    - Made very minor sentence changes to README

--- ubuntu/2-codingtools.sh
+++ ubuntu/2-codingtools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Summary:
-# This is a script that just a bunch of the installs I normally use 
+# This is a script that just a bunch of the installs I normally use
 # on ubuntu machines. I got tired of blowing up my VMs and going through
 # This manually. I'm wondering at this point if I just use NixOS.
 
@@ -138,6 +138,7 @@ function install_terraform() {
     sudo tee /etc/apt/sources.list.d/hashicorp.list
     sudo apt update -y
     sudo apt install terraform -y
+    brew install tfenv
   else
     write_log $lf "SKIPPING - Terraform installed"
     echo $(terraform -version)
@@ -181,13 +182,13 @@ function install_standard_apt_pkg() {
 write_log $lf "START Script"
 
 write_log $lf "Running apt update/upgrade"
-sudo apt update -y && sudo apt upgrade -y 
+sudo apt update -y && sudo apt upgrade -y
 
 install_standard_apt_pkg
 
 # Installs that need APT repos or downloaded binaries
 install_rust
-install_golang "1.22.0"
+install_golang "1.23.6"
 # change_golang_version "1.22.0"
 install_kubectl
 install_dotnet "8.0"
